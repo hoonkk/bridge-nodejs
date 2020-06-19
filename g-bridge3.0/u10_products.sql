2495f96ac65e508d68344b4f56fcaea248206a23
hoonkk <byul2sol@naver.com>
2020-06-19 21:44:04 +0900

    sql dump

---
+++ g-bridge3.0/u10_products.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.17  Distrib 10.3.17-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: bridge
+-- ------------------------------------------------------
+-- Server version	10.3.17-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `u10_users`
+--
+
+DROP TABLE IF EXISTS `u10_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `u10_users` (
+  `uid` varchar(20) NOT NULL,
+  `pass` varchar(128) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `point` int(11) DEFAULT NULL,
+  `phone` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `u10_users`
+--
+
+LOCK TABLES `u10_users` WRITE;
+/*!40000 ALTER TABLE `u10_users` DISABLE KEYS */;
+INSERT INTO `u10_users` VALUES ('admin','42zh6Igw2kg=','admin',10000000,'gsG+9HPaSLo='),('byul2sol@naver.com','G8kDGEDrMpg=','김경훈',450000,'gsG+9HPaSLo='),('myshark93@daum.net','gsG+9HPaSLo=','홍이랑',3000000,'zppZDIh4zR8='),('andora@naver.com','gsG+9HPaSLo=','임미주',10000000,'Kc1KZ/AcWlM=');
+/*!40000 ALTER TABLE `u10_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-19 21:43:48
